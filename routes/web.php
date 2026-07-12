2f1d701070bec877d46ed034f4c6f94916176ccf
Mohamed Selim <113633078+selimwdev@users.noreply.github.com>
2026-07-12 21:46:03 +0200

    new feature 1

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/generate', [GenerateController::class, 'show']);
 Route::post('/generate', [GenerateController::class, 'generate'])->name('generate');
 Route::post('/generate-many1', [GenerateController::class, 'generateMany'])->name('generate.many');
 
-#
+#secret
